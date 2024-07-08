f1d08bbb6ed53d737f33b6ed0e619bd50278d8fb
hotcat0421 <musicboxy11@gmail.com>
2024-07-08 22:30:40 +0900

    Initial Commit

---
+++ Homework4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Homework4
+//
+//  Created by 박정우 on 7/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Homework4/Homework4App.swift
@@ -0,0 +1,17 @@
+//
+//  Homework4App.swift
+//  Homework4
+//
+//  Created by 박정우 on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Homework4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
